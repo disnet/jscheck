4042fd48ebc686c1268b0fc12272294cb78d7fd9
disnet <tim.disney@gmail.com>
2010-02-09 22:18:14 -0800

    playing around with some parsec

---
+++ src/Parser.hs
@@ -0,0 +1,11 @@
+import Text.ParserCombinators.Parsec
+
+simple :: Parser Char
+simple = letter
+
+run :: Show a => Parser a -> String -> IO ()
+run p input 
+  = case (parse p "" input) of
+    Left err -> do {putStr "parse error at "; print err}
+    Right x -> print x
+  
