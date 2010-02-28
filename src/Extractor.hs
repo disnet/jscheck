1d7e8cff161f2aadfcb62164def0c5f031df8055
disnet <tim.disney@gmail.com>
2010-02-27 18:45:16 -0800

    adding fuction that should allow us to add fields to an existing xtype

--- src/Extractor.hs
+++ src/Extractor.hs
@@ -1,15 +1,16 @@
 module Extractor where
 import HJS.Parser
 import HJS.Parser.JavaScript
+import Data.List
 
 data XType = XType { typeName :: String
                    , typeFields :: [String] } deriving (Show, Eq)
 
 class ExtractC t where
-  extract :: t -> [XType]
+  extract :: t -> [XType] -> [XType]
 
 instance ExtractC SourceElement where
-  extract (Stmt s) = extract s
+  extract (Stmt s) c = extract s c
 --  extract (FuncDecl fd) = extract fd
 
 
@@ -17,35 +18,47 @@ instance ExtractC MemberExpr where
 --  extract (MemPrimExpr p) = extract p
 --  extract (ArrayExpr me e) = []
 --  extract (MemberNew me e) = []
-  extract (MemberCall me e) = 
+  extract (MemberCall me e) c = 
     case me of
       MemberCall me "prototype" -> case me of
-          MemPrimExpr (Ident s) -> [XType {typeName=s, typeFields=[e]}]
+          MemPrimExpr (Ident s) -> insert_xtype XType {typeName=s, typeFields=[e]} c
           _ -> []
       _ -> []
 
+insert_xtype ::  XType -> [XType] -> [XType]
+insert_xtype xtype current_list = 
+  if any (\p -> new_name == (typeName p)) current_list
+    then map (\oldtype -> 
+      if (typeName oldtype) == new_name 
+        then XType {typeName=new_name, typeFields=(typeFields oldtype) ++ (new_fields)}
+        else oldtype) current_list
+      else current_list ++ [xtype]
+    where new_name = (typeName xtype) 
+          new_fields = (typeFields xtype)
+
+
 instance ExtractC Stmt where
-  extract (StmtPos p s) = extract s
+  extract (StmtPos p s) c = extract s c
 
 instance ExtractC Expr where
-  extract (AssignE p) = extract p
+  extract (AssignE p) c = extract p c
 
 instance ExtractC AssignE where
-  extract (Assign left AssignNormal right) = (extract left) ++ (extract right)
-  extract (Assign left op right) = (extract left) ++ (extract right)
-  extract _ = []
+  extract (Assign left AssignNormal right) c = (extract left c) ++ (extract right c)
+  extract (Assign left op right) c = (extract left c) ++ (extract right c)
+  extract _ _ = []
 
 
 
 instance ExtractC LeftExpr where
-  extract (CallExpr c) = extract c
+  extract (CallExpr x) c = extract x c
 
 instance ExtractC CallExpr where
-  extract (CallPrim p) = extract p
+  extract (CallPrim p) c = extract p c
  -- extract (CallMember m args)
 
 instance ExtractC Stmt' where
-  extract (ExprStmt p) = extract p
+  extract (ExprStmt p) c = extract p c
 --  extract (IfStmt p) = extract p
 --  extract (Block xs) = extract xs
 --  extract (ItStmt p) = extract p
@@ -61,6 +74,6 @@ instance ExtractC Stmt' where
 
 
 runExtractor :: [SourceElement] -> [XType]
-runExtractor (x:xs) = (extract x) ++ (runExtractor xs)
+runExtractor (x:xs) = (extract x []) ++ (runExtractor xs)
 runExtractor _ = []
 
