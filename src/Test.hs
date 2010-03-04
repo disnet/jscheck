c483f45f80e34df04257c68011d487554511c156
Huascar A. Sanchez <huascarsanchez@Huascar-A-Sanchezs-MacBook-Pro.local>
2010-03-04 15:06:06 -0800

    Updated Checker to support typechecking of statements. Test CheckC was updated to avoid any conflits with the CheckC in the Checker file.

--- src/Test.hs
+++ src/Test.hs
@@ -9,23 +9,23 @@ import Data.List
 
 
 -- bad example for unit test but it illustrates a way to walk the AST
-class CheckC t where
-  check :: t -> Assertion
+class CheckC' t where
+  check' :: t -> Assertion
 
-instance CheckC SourceElement where
-  check (Stmt s) = check s
-  check fd = check fd
+instance CheckC' SourceElement where
+  check' (Stmt s) = check' s
+  check' fd = check' fd
 
-instance CheckC Stmt where
-  check (StmtPos p s) = check s
+instance CheckC' Stmt where
+  check' (StmtPos p s) = check' s
 
-instance CheckC Stmt' where
-  check (VarStmt (v:vs)) = check v
-  check (EmptyStmt) = assertBool "do nothing" True
+instance CheckC' Stmt' where
+  check' (VarStmt (v:vs)) = check' v
+  check' (EmptyStmt) = assertBool "do nothing" True
 
-instance CheckC VarDecl where
-  check (VarDecl s (Just e)) = assertBool "do nothing" True
-  check (VarDecl s Nothing)  = assertEqual "var should be 'x'" s "x"
+instance CheckC' VarDecl where
+  check' (VarDecl s (Just e)) = assertBool "do nothing" True
+  check' (VarDecl s Nothing)  = assertEqual "var should be 'x'" s "x"
 
 
 main = defaultMain tests
@@ -47,7 +47,7 @@ parse_fail p f = case parseProgram p of
                       Left l -> assertFailure $ "Parse error: " ++ (show l)
 
 -- Right [Stmt (StmtPos (1,1) (VarStmt [VarDecl "x" Nothing])),Stmt (StmtPos (1,6) EmptyStmt)]
-test_simple = parse_fail "var x;" (\p -> check (head p))
+test_simple = parse_fail "var x;" (\p -> check' (head p))
 
 extract_single_method = parse_fail "Foo.prototype.bar = function(a) {return 0;};" (\p ->
     let xtype = head (runExtractor p) in
